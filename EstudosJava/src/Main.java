7c19b91338f19e94b838d633dd86afd222117a0b
pedro-hbraga <braga-pedro@hotmail.com>
2023-04-01 16:52:30 -0300

    Primeiro commit teste na branch

--- EstudosJava/src/Main.java
+++ EstudosJava/src/Main.java
@@ -1,5 +1,36 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var pathFile = "E:\\Cursos\\Alura\\Java\\IO\\EstudosJava\\Arquivos\\lorem.txt";
+
+        try {
+            // Fluxo (Stream) de entrada de um arquivo
+            FileInputStream fs = new FileInputStream(pathFile);
+
+            // .read() do fs retorna em bytes, nao desejado
+            //InputStreamReader transforma bytes em caracteres
+            InputStreamReader inputReader = new InputStreamReader(fs);
+            //Input nao consegue agrupar linhas inteiras, lendo byte a byte, sendo necessario um buffer para salvar
+            // cada caractere
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+
+            var line = bufferedReader.readLine();
+            while(line != null ){
+
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
